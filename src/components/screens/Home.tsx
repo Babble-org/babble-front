142540aa62995ed75d326a9f705a5992a2cfb6a3
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-04 21:46:24 +0900

    Home 스크린 작성 및 App.tsx 수정

---
+++ src/components/screens/Home.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+const Container = styled.View``;
+const Text = styled.Text``;
+
+const Home = () => {
+  return (
+    <Container>
+      <Text>Home</Text>
+    </Container>
+  );
+};
+
+export default Home;
