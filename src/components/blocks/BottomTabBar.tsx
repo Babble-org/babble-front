f7210ec1b1d47585de9bd110277ec4cc5e8fd23b
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-08 20:33:05 +0900

    feat/BottomTabBar 삭제 및 컴포넌트로 대체

---
+++ src/components/blocks/BottomTabBar.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+
+const TabContainer = styled.View`
+  background-color: beige;
+`;
+
+const BottomTabBar = () => {
+  return <TabContainer></TabContainer>;
+};
+
+export default BottomTabBar;
--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,11 +1,12 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../components/screens/Home";
+import BottomTabBar from "../components/blocks/BottomTabBar";
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator screenOptions={{ headerShown: false }} tabBar={BottomTabBar}>
       <Tab.Screen name="Home" component={Home} />
     </Tab.Navigator>
   );
