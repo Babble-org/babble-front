54337014602abe5ef94f2778bd59c8c83c851cb1
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-09 16:57:05 +0900

    feat/context 파일 분리

---
+++ src/utils/context.tsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+import { InsetsContextInterface } from "./index";
+
+export const InsetsContext = createContext<InsetsContextInterface>({
+  insets: {
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  setInsets: (insets) => {},
+});
