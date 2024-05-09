490dea7b3ddd464f2e51a22bd1ab8fefe1391c69
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-09 17:36:07 +0900

    feat/inset을 받아와 bottomTabBar에 적용

--- src/components/blocks/BottomTabBar.tsx
+++ src/components/blocks/BottomTabBar.tsx
@@ -1,11 +1,16 @@
 import styled from "styled-components/native";
+import { useContext } from "react";
+import { InsetsContext } from "../../utils/context";
+import { Insets } from "../../utils";
 
 const TabContainer = styled.View`
   background-color: beige;
+  height: ${(props: { insets: Insets }) => 60 + props.insets.bottom}px;
 `;
 
 const BottomTabBar = () => {
-  return <TabContainer></TabContainer>;
+  const { insets } = useContext(InsetsContext);
+  return <TabContainer insets={insets}></TabContainer>;
 };
 
 export default BottomTabBar;
