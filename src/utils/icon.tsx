582c9c5540e4ab116f8c35557fae12064996fb85
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-09 19:29:03 +0900

    feat/BottomTab을 위한 Icon 축가

--- src/utils/icon.tsx
+++ src/utils/icon.tsx
@@ -1,13 +1,8 @@
 import { WithLocalSvg } from "react-native-svg/css";
 import BabbleIconSVG from "../../assets/svg/babble_logo.svg";
 import ProfileIconSVG from "../../assets/svg/profile.svg";
-
-interface IconProps {
-  height?: number;
-  width?: number;
-  size?: number;
-  color?: string;
-}
+import { IconProps } from "./index";
+import { Ionicons } from "@expo/vector-icons";
 
 const BabbleIcon = ({ height, width, color }: IconProps) => {
   return (
@@ -31,8 +26,59 @@ const ProfileIcon = ({ height, width, color }: IconProps) => {
   );
 };
 
+const MailIcon = ({ size, color, outline }: IconProps) => {
+  return (
+    <Ionicons
+      name={outline ? "mail-outline" : "mail"}
+      size={size}
+      color={color}
+    />
+  );
+};
+const HomeIcon = ({ size, color, outline }: IconProps) => {
+  return (
+    <Ionicons
+      name={outline ? "home-outline" : "home"}
+      size={size}
+      color={color}
+    />
+  );
+};
+const SearchIcon = ({ size, color, outline }: IconProps) => {
+  return (
+    <Ionicons
+      name={outline ? "search-outline" : "search"}
+      size={size}
+      color={color}
+    />
+  );
+};
+const HeartIcon = ({ size, color, outline }: IconProps) => {
+  return (
+    <Ionicons
+      name={outline ? "heart-outline" : "heart"}
+      size={size}
+      color={color}
+    />
+  );
+};
+const MicIcon = ({ size, color, outline }: IconProps) => {
+  return (
+    <Ionicons
+      name={outline ? "mic-outline" : "mic"}
+      size={size}
+      color={color}
+    />
+  );
+};
+
 declare var Icon: any;
 export default Icon = {
   BabbleIcon,
   ProfileIcon,
+  MailIcon,
+  HomeIcon,
+  SearchIcon,
+  HeartIcon,
+  MicIcon,
 };
