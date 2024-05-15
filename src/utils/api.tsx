d9b9d30b9c5994f1d865dc9e8fe0ec127ea3ab85
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-15 18:14:08 +0900

    fix/api 수정

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,13 +1,11 @@
 import axios from "axios";
-const BASE_URL = "https://babble-staging.gigalixirapp.com/openapi/api";
+const BASE_URL = "https://babble-staging.gigalixirapp.com/api";
 
 const check_username = async (username: string) => {
   try {
-    const response = await axios.get(`${BASE_URL}/users/check_username`, {
-      params: {
-        user_name: username,
-      },
-    });
+    const response = await axios.get(
+      `${BASE_URL}/users/check_username/${username}`,
+    );
     return response.data;
   } catch (error) {
     console.error(error);
