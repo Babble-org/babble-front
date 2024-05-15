36a93582d7381161fc89076a5b7cedae2e42577f
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-15 17:56:13 +0900

    feat/medium button 기능 추가

--- src/components/atoms/MediumButton.tsx
+++ src/components/atoms/MediumButton.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { TouchableOpacityProps } from "react-native";
 
 const BtnContainer = styled.TouchableOpacity`
   width: 100%;
@@ -7,17 +8,30 @@ const BtnContainer = styled.TouchableOpacity`
   border-radius: 30px;
   align-items: center;
   justify-content: center;
+  opacity: ${(props: { disabled: boolean }) => (props.disabled ? 0.5 : 1)};
 `;
 const Text = styled.Text`
   color: #fff;
-  font-size: 18px;
+  font-size: ${(props: { fontSize: number }) => props.fontSize}px;
   font-weight: 500;
 `;
 
-const MediumButton = ({ text, onPress }: { text: string; onPress: any }) => {
+const MediumButton = ({
+  text,
+  onPress,
+  fontSize,
+  enable,
+}: TouchableOpacityProps & {
+  text: string;
+  fontSize?: number;
+  enable?: boolean;
+}) => {
   return (
-    <BtnContainer onPress={onPress}>
-      <Text>{text}</Text>
+    <BtnContainer
+      onPress={onPress}
+      disabled={enable === undefined ? false : !enable}
+    >
+      <Text fontSize={fontSize ? fontSize : 18}>{text}</Text>
     </BtnContainer>
   );
 };
