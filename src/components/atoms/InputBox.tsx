559449551589c8069de424f89f684dd1aed6412b
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-15 17:56:02 +0900

    feat/inputbox 컴포넌트 기능 추가

--- src/components/atoms/InputBox.tsx
+++ src/components/atoms/InputBox.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import { TextInputProps } from "react-native";
 
 const InputContainer = styled.TextInput`
   width: 100%;
@@ -6,10 +7,23 @@ const InputContainer = styled.TextInput`
   border-bottom-width: 1px;
   border-bottom-color: #d9d9d9;
   margin: 10px 0;
+  opacity: ${(props: { editable: boolean }) => (props.editable ? 1 : 0.5)};
 `;
 
-const InputBox = ({ placeholder }: { placeholder: string }) => {
-  return <InputContainer placeholder={placeholder}></InputContainer>;
+const InputBox = ({
+  placeholder,
+  enable,
+  onChangeText,
+  value,
+}: TextInputProps & { enable?: boolean }) => {
+  return (
+    <InputContainer
+      placeholder={placeholder}
+      editable={enable === undefined ? true : enable}
+      onChangeText={onChangeText}
+      value={value}
+    ></InputContainer>
+  );
 };
 
 export default InputBox;
