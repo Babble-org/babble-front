75e2edab45ff1c55ee26b61d04439c0a6b7839f9
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-15 14:50:35 +0900

    feat/초대코드 페이지 제작

--- src/components/screens/register/InviteCodePage.tsx
+++ src/components/screens/register/InviteCodePage.tsx
@@ -1,8 +1,39 @@
 import styled from "styled-components/native";
+import RegisterHeader from "../../blocks/headers/RegisterHeader";
+import InputBox from "../../atoms/InputBox";
+import MediumButton from "../../atoms/MediumButton";
 
-const Container = styled.View``;
+const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  padding: 0 30px;
+`;
+const ContentWrap = styled.View`
+  margin-top: 20px;
+`;
+const Text = styled.Text`
+  font-size: 25px;
+  font-weight: 800;
+`;
+const BtnContainer = styled.View`
+  width: 100%;
+  margin-top: 20px;
+  align-items: center;
+  justify-content: center;
+`;
 const InviteCodePage = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <RegisterHeader></RegisterHeader>
+      <ContentWrap>
+        <Text>초대코드 입력하기</Text>
+        <InputBox placeholder={"초대코드 입력"}></InputBox>
+        <BtnContainer>
+          <MediumButton text={"다음"} onPress={() => null} />
+        </BtnContainer>
+      </ContentWrap>
+    </Container>
+  );
 };
 
 export default InviteCodePage;
