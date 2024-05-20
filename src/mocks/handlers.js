ee544d58da6a6645cff4cfab209191c032d6ed56
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-20 23:32:22 +0900

    feat/바브 받아오는 핸들러 작성

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,11 +1,20 @@
 import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.get("http://localhost:8081/", () => {
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
-    });
+  http.get("http://localhost:8081/babb", () => {
+    return HttpResponse.json([
+      {
+        id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
+        nick_name: "박건태",
+        inserted_at: "2017-09-12T12:34:55Z",
+        content: "안녕하세요. 저는 박건태입니다.",
+      },
+      {
+        id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
+        nick_name: "이재혁",
+        inserted_at: "2017-09-12T12:34:55Z",
+        content: "안녕하세요. 저는 이재혁입니다.",
+      },
+    ]);
   }),
 ];
