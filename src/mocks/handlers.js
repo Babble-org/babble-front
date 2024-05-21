5d00a04dd5b79aac8ae8e6761812e66d544b3070
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-21 17:16:06 +0900

    feat/ 핸들러에 이미지 배열 작성

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -6,14 +6,34 @@ export const handlers = [
       {
         id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
         nick_name: "박건태",
-        inserted_at: "2017-09-12T12:34:55Z",
+        inserted_at: "2024-05-20T12:34:55Z",
         content: "안녕하세요. 저는 박건태입니다.",
+        img: [
+          "https://picsum.photos/200/300",
+          "https://picsum.photos/200/300",
+          "https://picsum.photos/200/300",
+        ],
       },
       {
         id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3e",
         nick_name: "이재혁",
-        inserted_at: "2017-09-12T12:34:55Z",
+        inserted_at: "2024-05-20T12:34:55Z",
         content: "안녕하세요. 저는 이재혁입니다.",
+        img: ["https://picsum.photos/200/300", "https://picsum.photos/200/300"],
+      },
+      {
+        id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3c",
+        nick_name: "이재혁",
+        inserted_at: "2024-05-20T12:34:55Z",
+        content: "안녕하세요. 저는 이재혁입니다.",
+        img: ["https://picsum.photos/200/300"],
+      },
+      {
+        id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3a",
+        nick_name: "이재혁",
+        inserted_at: "2024-05-20T12:34:55Z",
+        content: "안녕하세요. 저는 이재혁입니다.",
+        img: [],
       },
     ]);
   }),
