51a0735e783a14d181737ed76b3526e765463d14
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-23 20:11:48 +0900

    feat/ 이미지 상세보기 컴포넌트 작성

---
+++ src/components/atoms/DetailImage.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components/native";
+import { DetailImageProps } from "../../utils";
+
+const ImageBox = styled.Modal`
+  opacity: 0.5;
+`;
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+  opacity: 0.5;
+`;
+const ModalContainer = styled.Pressable`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ImageContainer = styled.View`
+  width: 80%;
+  height: 80%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledImage = styled.Image`
+  width: 90%;
+  height: 300px;
+`;
+
+const DetailImage = ({ visible, setVisible, img }: DetailImageProps) => {
+  return (
+    <ImageBox visible={visible} transparent animationType="fade">
+      <ModalContainer onPress={() => setVisible(false)}>
+        <ImageContainer>
+          <StyledImage
+            source={{ uri: img }}
+            resizeMode={"stretch"}
+          ></StyledImage>
+        </ImageContainer>
+      </ModalContainer>
+    </ImageBox>
+  );
+};
+
+export default DetailImage;
