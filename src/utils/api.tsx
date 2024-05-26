c59436a2502df7a970428715a06bcbbf7cab69ac
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-26 23:23:50 +0900

    feat/ login, register 쿼리 함수 작성 및 타입 추가

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { LoginInfo, RegisterInfo } from ".";
 const BASE_URL = "https://babble-staging.gigalixirapp.com/api";
 
 const check_username = async (username: string) => {
@@ -14,12 +15,41 @@ const check_username = async (username: string) => {
 
 const getBabb = async () => {
   try {
-    const response = await axios.get(`http://localhost:8081/babb`);
+    const response = await axios.get(`http://localhost:8081/babb`, {});
     return response.data;
   } catch (error) {
     console.error(error);
   }
 };
 
-const api = { check_username, getBabb };
+const Register = async (registerInfo: RegisterInfo | null) => {
+  try {
+    console.log(registerInfo);
+    const response = await axios.post(`${BASE_URL}/users`, registerInfo, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+const Login = async (loginInfo: LoginInfo | null) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/auth/login`, loginInfo, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+const api = { check_username, getBabb, Register, Login };
 export default api;
