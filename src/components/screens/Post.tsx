b35833e38c8089bedb6e5376ede9dad3a92c1f58
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2024-05-30 18:34:12 +0900

    feat/ 이미지를 바이너리로 전송

--- src/components/screens/Post.tsx
+++ src/components/screens/Post.tsx
@@ -4,6 +4,8 @@ import DetailHeader from "../blocks/headers/DetailHeader";
 import BottomTabBar from "../blocks/BottomTabBar";
 import PostBox from "../blocks/PostBox";
 import { useState } from "react";
+import * as FileSystem from "expo-file-system";
+import api from "../../utils/api";
 
 const Container = styled.View`
   flex: 1;
@@ -15,16 +17,44 @@ const ContentContainer = styled.View`
 
 const Post = () => {
   const [isRecording, setIsRecording] = useState(false);
+  const [imageData, setImageData] = useState<object[] | null>(null);
+
+  const sendBtnOnPress = async () => {
+    imageData?.map(async (data: any) => {
+      if (data !== undefined) {
+        // 이미지의 타입으로 presignedURL 요청
+        const presignedURL = await api.getPresignedURL(data.type);
+        console.log(presignedURL.path);
+        try {
+          // presignedURL을 이용해 이미지 업로드
+          const response = await FileSystem.uploadAsync(
+            presignedURL.url,
+            data.uri,
+            {
+              fieldName: data.name,
+              httpMethod: "PUT",
+              uploadType: FileSystem.FileSystemUploadType.BINARY_CONTENT,
+            }
+          );
+          console.log(response);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    });
+  };
+
   return (
     <Container>
       <DetailHeader title="게시" />
       <ContentContainer>
-        <PostBox isRecording={isRecording} />
+        <PostBox isRecording={isRecording} setImageData={setImageData} />
       </ContentContainer>
       <BottomTabBar
         isPostMode={true}
         isRecording={isRecording}
         setIsRecording={setIsRecording}
+        sendBtnOnPress={sendBtnOnPress}
       />
     </Container>
   );
